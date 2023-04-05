0372b26a392e36d38c64aa1609ffb361d89d9d57
AnnaStevenon <arstevenson@wisc.edu>
2023-04-05 10:10:27 -0500

    Activity 9
    Added background color

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ window.onload = setMap();
 
 //set up map
 function setMap(){
-    var width = 460,
-        height = 460;
+    var width = 500,
+        height = 500;
     //create new svg container for the map
     var map = d3.select("body")
         .append("svg")
@@ -17,20 +17,21 @@ function setMap(){
         .center([-89, 44]) //long, lat
         .rotate([1,0,0.75]) //long, lat, angle
         .parallels([43,45]) //specficic to coninc projection, two latitudes define top and bottom
-        .scale(5500) //pixel scale or zoom level
+        .scale(5800) //pixel scale or zoom level
         .angle(62) //trying to make it look normal
         .translate([width/2, height/2]) //how big map is in the frame
 
     //define path. Path is a way to convert projection into a usable object
     var path = d3.geoPath()
         .projection(projection);
-
+    //loading the data as promises
     var promises = [
         d3.csv("data/countiesData.csv"),
         d3.json("data/Counties.topojson"),
         d3.json("data/Midwest_states.topojson")
     ];
 
+    //apply callback to all promised data
     Promise.all(promises).then(callback);
 
     function callback(data){
