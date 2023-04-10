47be76db06ccc9fe404dcb1dc9fbc72215f7bf41
AnnaStevenon <arstevenson@wisc.edu>
2023-04-10 16:50:23 -0500

    Working on activity 10

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 // 4. a taste of responsive design
 
 // some sort of anonymous function to avoid global variables - end brackets at the end of the code
-(function () {
+(function (){
 
     //execute script when window is loaded
 
@@ -21,7 +21,7 @@
 
     //set up map
     function setMap(){
-        var width = 500,
+        var width = window.innerWidth * 0.45,
             height = 500;
         //create new svg container for the map
         var map = d3.select("body")
@@ -34,7 +34,7 @@
             .center([-89, 44]) //long, lat
             .rotate([1,0,0.75]) //long, lat, angle
             .parallels([43,45]) //specficic to coninc projection, two latitudes define top and bottom
-            .scale(5800) //pixel scale or zoom level
+            .scale(6100) //pixel scale or zoom level
             .angle(62) //trying to make it look normal
             .translate([width/2, height/2]) //how big map is in the frame
 
@@ -57,7 +57,7 @@
                 states = data[2];
             //soft convert to use topojson format
             var Counties = topojson.feature(counties, counties.objects.Counties).features; //the .features part puts it in array format for loading each county one by one
-            var midStates = topojson.feature(states, states.objects.Midwest_states)
+            var midStates = topojson.feature(states, states.objects.Midwest_states);
 
 
         //add States to the map all at once - would remove the .features 
@@ -65,23 +65,34 @@
                 .datum(midStates) //drawing all counties together
                 .attr("class", "states")
                 .attr("d", path); //setting "d" to the path variable, "d" defines the coordinates of the path - has nothing to do with d used in a function
-        
-            var WICounties = map.selectAll(".counties")
-                .data(Counties)
-                .enter()
-                .append("path")
-                .attr("class", function(d){
-                    return "counties " + d.properties.County;
-                })
-                .attr("d", path);
+
 
             Counties = joinData(Counties, csvData);
 
-            setEnumernationUnits(Counties, map, path);
+            var colorScale = makeColorScale(csvData);
 
-        }
+            setEnumerationUnits(Counties, map, path, colorScale);
 
-    }
+            setChart(csvData, colorScale);
+
+        };
+
+    };
+
+    //function to create coordinated bar chart
+    function setChart(csvData, colorScale){
+        //chart frame dimensions
+        var chartWidth = window.innerWidth * 0.45,
+            chartHeight = 500;
+
+        //create a second svg element to hold to bar chart
+        var chart = d3.select("body")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("class", "chart")
+            .style("background-color", "grey");
+    };
 
     function joinData(Counties, csvData) {
           // loop through csv to assign each set of csv attribute values to geojson county
@@ -101,15 +112,42 @@
                         var val = parseFloat(csvRegion[attr]); //get csv attribute value
                         geojsonProps[attr] = val; //assign aatribute and value to geojson properties
                     });
-                }
-            }
-        }
+                };
+            };
+        };
         console.log(Counties);
 
         return Counties;
     };
 
-    function setEnumernationUnits(Counties, map, path) {
+//function to create color scale generator
+    function makeColorScale(data){
+        var colorClasses = [
+            "#FEE5D9",
+            "#FCAE91",
+            "#FB6A4A",
+            "#DE2D26",
+            "#A50F15"
+        ];
+
+        //create color scale generator
+        var colorScale = d3.scaleQuantile()
+            .range(colorClasses);
+
+        //build array of all values of the expressed attribute
+        var domainArray = [];
+        for (var i=0; i<data.length; i++){
+            var val = parseFloat(data[i][expressed]);
+            domainArray.push(val);
+        };
+
+        //assign array of expressed values as scale domain
+        colorScale.domain(domainArray);
+
+        return colorScale;
+    };
+
+    function setEnumerationUnits(Counties, map, path, colorScale) {
         
         var WICounties = map
         .selectAll(".counties")
@@ -120,9 +158,11 @@
             return "counties " + d.properties.County;
         })
         .attr("d", path)
-        .style("fill", function(d){
-            return "red"; // not working?
-        })
+        .style("fill", function(d) {
+            if (d.properties[expressed] > 0) {
+            return colorScale(d.properties[expressed])}
+            else { return "gray"} // data with a zero means that data was not publishable/available for that county, there are no "zero" values that mean the yield or number of cows was zero.
+        });
     };
 
-})() // the end of the anonymous function at the begining of this code
+})(); // the end of the anonymous function at the begining of this code
